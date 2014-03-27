d2f49cfa4620e77ea4551d50236dee40ca74f32d
edurocher <edurocher0@gmail.com>
2014-03-27 13:53:47 +0100

    Comment out all browsers but Chrome to test travis build...

--- tests/intern.js
+++ tests/intern.js
@@ -11,15 +11,15 @@ define({
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
-		{ browserName: "internet explorer", version: "11", platform: "Windows 8.1" },
-		{ browserName: "internet explorer", version: "10", platform: "Windows 8" },
-		{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
-		{ browserName: "firefox", version: "25", platform: "Windows 7" },
-		{ browserName: "chrome", version: "", platform: "Windows 7" },
-		{ browserName: "safari", version: "6", platform: "OS X 10.8" },
+		//{ browserName: "internet explorer", version: "11", platform: "Windows 8.1" },
+		//{ browserName: "internet explorer", version: "10", platform: "Windows 8" },
+		//{ browserName: "internet explorer", version: "9", platform: "Windows 7" },
+		//{ browserName: "firefox", version: "25", platform: "Windows 7" },
+		{ browserName: "chrome", version: "", platform: "Windows 7" }//,
+		//{ browserName: "safari", version: "6", platform: "OS X 10.8" },
 
 		// Mobile
-		{ browserName: "iphone", platform: "OS X 10.9", version: "7"}
+		//{ browserName: "iphone", platform: "OS X 10.9", version: "7"}
 		// , { browserName: "android", platform: "Android" }		not currently working
 	],
 
